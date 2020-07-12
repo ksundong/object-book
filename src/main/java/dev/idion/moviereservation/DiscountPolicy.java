d60d03eb2215cbc1d9647cc4c9d74db0023e78a6
IdionKim <ksundong@gmail.com>
2020-07-12 18:10:44 +0900

    할인 정책 추상 클래스 생성
    할인 조건을 만족하는 경우에만 추상 메서드를 실행하도록 합니다.
    템플릿 메서드 패턴을 사용합니다.

---
+++ src/main/java/dev/idion/moviereservation/DiscountPolicy.java
@@ -0,0 +1,25 @@
+package dev.idion.moviereservation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public abstract class DiscountPolicy {
+
+  private List<DiscountCondition> conditions = new ArrayList<>();
+
+  public DiscountPolicy(DiscountCondition... conditions) {
+    this.conditions = Arrays.asList(conditions);
+  }
+
+  public Money calculateDiscountAmount(Screening screening) {
+    for (DiscountCondition each : conditions) {
+      if (each.isSatisfiedBy(screening)) {
+        return getDiscountAmount(screening);
+      }
+    }
+
+    return Money.ZERO;
+  }
+
+  protected abstract Money getDiscountAmount(Screening screening);
+}
